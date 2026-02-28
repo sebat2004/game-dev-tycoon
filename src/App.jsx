aaddd958b79d025a1d62fa76d34747ee1720aa21
Carson Secrest <carsonsecrest@gmail.com>
2026-02-28 11:33:43 -0800

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Home from './pages/Home'
 import Game from './pages/Game'
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/game/:roomId" element={<Game />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/game/:roomId" element={<Game />} />
+            </Routes>
+            <Analytics />
+        </>
     )
 }
